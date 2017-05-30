f19ec2db0bc861191c8164b7f960a6d4a0a71611
scavet64 <scavet64@students.rowan.edu>
2017-05-30 18:24:46 -0400

    Rename sink ship, edit prefab

---
+++ Assets/Scripts/SinkShip.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinkShip : MonoBehaviour {
+    Rigidbody rigid;
+    Buoyancy buoyance;
+
+	// Use this for initialization
+	void Start () {
+        rigid = gameObject.GetComponent<Rigidbody>();
+        buoyance = gameObject.GetComponent<Buoyancy>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	}
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.GetComponent<Cannonball>() != null) {
+            Sink();
+        }
+    }
+
+    private void Sink() {
+        rigid.AddTorque(transform.right * -500);
+        rigid.AddForce(new Vector3(0, 1000, 0));
+        Destroy(gameObject.GetComponent<Buoyancy>());
+        Destroy(gameObject, 2);
+    }
+}
